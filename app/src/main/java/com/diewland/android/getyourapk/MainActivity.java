ccb2ed18072a691d0b68ae25bdd8e5753256f973
Yuwat Chuesathuchon <yuwat@mfec.co.th>
2017-08-21 15:35:31 +0700

    ignore url explosion, order app by name

--- app/src/main/java/com/diewland/android/getyourapk/MainActivity.java
+++ app/src/main/java/com/diewland/android/getyourapk/MainActivity.java
@@ -5,8 +5,7 @@ import android.content.pm.ApplicationInfo;
 import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
+import android.os.StrictMode;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
@@ -15,15 +14,18 @@ import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.TextView;
 
 import java.io.File;
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
 
 public class MainActivity extends AppCompatActivity {
 
-    final ArrayList<String> appList = new ArrayList<String>();
     private ListView lv ;
     private ArrayAdapter<String> listAdapter ;
+    private HashMap<String, String> appHash = new HashMap<String, String>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,17 +34,27 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        // Ignore detect file URI exposure
+        // https://stackoverflow.com/a/40674771/466693
+        StrictMode.VmPolicy.Builder builder = new StrictMode.VmPolicy.Builder();
+        StrictMode.setVmPolicy(builder.build());
+
         // Find the ListView resource.
         lv = (ListView) findViewById( R.id.list_row );
 
-        // list all apps
+        // Collect all apps
         PackageManager pm = getPackageManager();
         for (ApplicationInfo app : pm.getInstalledApplications(0)) {
-            appList.add(app.publicSourceDir);
+            String app_name = (String)pm.getApplicationLabel(app);
+            appHash.put(app.publicSourceDir, app_name);
         }
 
+        // Sort by app_name
+        ArrayList appNames = new ArrayList<String>(appHash.values());
+        Collections.sort(appNames, String.CASE_INSENSITIVE_ORDER);
+
         // Create ArrayAdapter using the planet list.
-        listAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_expandable_list_item_1, appList);
+        listAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_expandable_list_item_1, appNames);
 
         // Set the ArrayAdapter as the ListView's adapter.
         lv.setAdapter( listAdapter );
@@ -51,16 +63,21 @@ public class MainActivity extends AppCompatActivity {
         lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> adapterView, View v, int pos, long l) {
-                String apkPath = appList.get(pos);
-                File file = new File(apkPath);
-                final Intent intent = new Intent(android.content.Intent.ACTION_SEND);
-                intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-                intent.putExtra(Intent.EXTRA_STREAM, Uri.fromFile(file));
-                intent.setType("file/*");
-                startActivity(intent);
+                String curAppName = ((TextView)v).getText().toString();
+                for(String apkPath : appHash.keySet()){
+                    if(appHash.get(apkPath).equals(curAppName)){
+                        File file = new File(apkPath);
+                        final Intent intent = new Intent(android.content.Intent.ACTION_SEND);
+                        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                        intent.putExtra(Intent.EXTRA_STREAM, Uri.fromFile(file));
+                        intent.setType("file/*");
+                        startActivity(intent);
+                    }
+                }
             }
         });
 
+        /*
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -69,6 +86,7 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+        */
     }
 
     @Override
